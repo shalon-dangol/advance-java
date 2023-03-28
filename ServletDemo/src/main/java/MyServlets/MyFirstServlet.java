afd0f9cb35b715022ea6e056606def1c27626fda
Shalon Dangol <shalondangol.sd@gmail.com>
2023-03-28 18:29:55 +0545

    updated

---
+++ ServletDemo/src/main/java/MyServlets/MyFirstServlet.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MyServlets;
+import jakarta.servlet.*;
+import java.io.*;
+//import javax.servlet.*;
+/**
+ *
+ * @author Shalon
+ */
+public class MyFirstServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig sc) throws ServletException {
+        
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return this.getServletConfig();
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("this is  a servlet page");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "random";
+    }
+
+    @Override
+    public void destroy() {
+        
+    }
+    
+}

6eddd756cc34a227777ecd337c5419ed8b521315
Shalon Dangol <shalondangol.sd@gmail.com>
2023-03-30 11:25:54 +0545

    updated

---
+++ ServletDemo/src/main/java/MyServlets/GenericServletDemo.java
@@ -0,0 +1,16 @@
+
+package MyServlets;
+
+import jakarta.servlet.*;
+import java.io.IOException;
+
+public class GenericServletDemo extends GenericServlet{
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+//       res.setContentType("text/html");
+//       res.getWriter().println ("hello i am from generic ");
+       
+    }
+    
+}
---
+++ ServletDemo/src/main/java/MyServlets/HttpServletDemo.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MyServlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author Shalon
+ */
+public class HttpServletDemo extends HttpServlet{
+
+    
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+      
+       res.setContentType("text/html");
+       int num1 =  Integer.parseInt(req.getParameter("num1"));
+       int num2 =  Integer.parseInt(req.getParameter("num2"));
+       int sum = num1 + num2;
+       PrintWriter out = res.getWriter();
+       out .println(sum);
+    }
+    
+    
+}
